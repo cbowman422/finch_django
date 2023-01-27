457af9b98c176d22a6dcd0636186d7fbf9f847f8
cbowman422 <chrisb0wman@hotmail.com>
2023-01-27 06:43:41 -0500

    search added to views.py finch list

--- main_app/views.py
+++ main_app/views.py
@@ -52,6 +52,13 @@ class FinchList(TemplateView):
   template_name = "finch_list.html"
 
   def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    context["finches"] = Finch.objects.all() # this is where we add the key into our context object for the view to use
-    return context
+        context = super().get_context_data(**kwargs)
+        # to get the query parameter we have to acccess it in the request.GET dictionary object        
+        name = self.request.GET.get("name")
+        # If a query exists we will filter by name 
+        if name != None:
+            # .filter is the sql WHERE statement and name__icontains is doing a search for any name that contains the query param
+            context["finches"] = Finch.objects.filter(name__icontains=name)
+        else:
+            context["finches"] = Finch.objects.all()
+        return context
